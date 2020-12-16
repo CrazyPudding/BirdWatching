a78bbce07d7321b1cf1520d2fe2bcfc241560f37
crazypudding <puddingleng@gmail.com>
2020-12-16 23:17:33 +0800

    Initial Commit

---
+++ BirdWatching/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BirdWatching
+//
+//  Created by Zehua on 2020/12/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BirdWatching/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BirdWatching
+//
+//  Created by Zehua on 2020/12/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
